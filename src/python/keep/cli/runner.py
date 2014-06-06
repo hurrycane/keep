6aef04c077238e12bdc519c3e7f44d5590509113
Bogdan Gaza <bc.gaza@gmail.com>
2014-06-07 01:22:22 +0300

    Initial structure for keep cli

--- src/python/keep/cli/runner.py
+++ src/python/keep/cli/runner.py
@@ -1,72 +1,65 @@
 from twitter.common import app, log
 from twitter.common.log.options import LogOptions
 
+import os
 import socket
 
-import pinject
-import gevent
+import etcd
 
-app.add_option('--with-ui', action="store_true", dest='with_ui',
-               default=False, help="Enable UI for this node")
+DISCOVERY_TOKEN = "DISCOVERY_TOKEN"
+ETCD_SEED = "ETCD_SEED"
 
-app.add_option('--name', dest='hostname', default=socket.gethostname(),
-               help="Name of the node that is running")
+SUPPORTED_COMMANDS = [
+  "get_latest_tag",
+  "start_build",
+  "finish_build",
+  "fetch_etcd_keys"
+]
 
-app.add_option('--namespace', dest='namespace', default="prod",
-               help="Name of the node that is running")
+app.add_option('--get-latest-tag', dest='get_latest_tag',
+               default=False, help="Fetch latest tag for the given service name")
 
-app.add_option('--ui-bind', dest='ui_bind', default="0.0.0.0",
-               help="IP on which we bind the UI HTTP server")
+app.add_option('--current-version', dest='current_version',
+               default=False, help="Pass the current version of the build")
 
-app.add_option('--ui-port', dest='ui_port', default=8080,
-               help="The port on which the UI starts.")
+# --start-build service-andromeda-api --current-version 1.129
+app.add_option('--start-build', dest='start_build',
+               default=False, help="Start a build for a gen service")
 
-app.add_option('--etcd_host', dest='etcd_host', default="localhost",
-               help="Host for one of the etcd peers in the cluster")
+# --finish-build service-andromeda-api --current-version 1.129
+app.add_option('--finish-build', dest='finish_build',
+               default=False, help="Finish a build that is already in progress")
 
-app.add_option('--etcd_port', dest='etcd_port', default=4001,
-               help="Port for the etcd host")
+app.add_option('--fetch-etcd-keys', dest='fetch_etcd_keys',
+               default=False, help="Fetches an archive with the required keys\
+                                    to start an etcd cluster")
 
-app.add_option('--discovery', dest='discovery', default=None,
-               help="Token used for the discovery service")
+def get_etcd_client():
+  if DISCOVERY_TOKEN in os.environ:
+    return etcd.Client(discovery=os.environ[DISCOVERY_TOKEN])
 
-from actors import ActorContext
-from crow import CrowContext
+  if ETCD_SEED in os.environ:
+    return etcd.Client(peers=os.environ[ETCD_SEED].split(","))
 
-from keep.agent import KeepRpcActor
+  return etcd.Client(peers=["etcd1-us-east-1-aws.2o.com:4001"])
 
-from keep.actors.binding_specs import *
+def get_latest_tag(options, service_name):
+  etcd_client = get_etcd_client()
+
+def start_build(options, service_name):
+  pass
+
+def finish_build(options, service_name):
+  pass
+
+def fetch_etcd_keys(options, hostname):
+  pass
 
 def main(args, options):
-  # lock a build
-  # get latest version of a service
-  # compute the next version of a service
-  # finish build
-
-  injector = pinject.new_object_graph(
-    binding_specs=[
-      ActorEtcdBindingSpec(options.etcd_host, options.etcd_port,
-                           options.discovery),
-      CrowHostnameBindingSpec(options.hostname, options.namespace)
-    ]
-  )
-
-  actor_context = injector.provide(ActorContext)
-  crow_context = injector.provide(CrowContext)
-
-  # start actor
-  main_actor_ref = actor_context.actor_of(KeepRpcActor.props(
-    crow_context=crow_context
-  ))
-
-  crow_context.add_handler("member-join",
-                           lambda member: main_actor_ref.tell(member))
-  crow_context.add_handler("member-leave",
-                           lambda member: main_actor_ref.tell(member))
-
-  #crow.anounce()
-
-  gevent.wait()
+  for command in SUPPORTED_COMMANDS:
+    if hasattr(options, command) and getattr(options, command) != False:
+      method = globals()[command]
+      method(options, getattr(options, command))
 
 # no GLOG to disk at least for now
 LogOptions.set_disk_log_level('NONE')
