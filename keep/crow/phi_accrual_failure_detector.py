cbf7d3a89105f1f2538a2d4a7b991a39e25af13a
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-15 20:00:07 +0300

    Latest iteration for the phi accural failure detector - most stable so far

--- keep/crow/phi_accrual_failure_detector.py
+++ keep/crow/phi_accrual_failure_detector.py
@@ -7,7 +7,7 @@ MATH_EXP_MAX = 709
 class AccuralFailureDetector(object):
 
   def __init__(self, history):
-    if len(history) < 10:
+    if len(history) < 3:
       raise NotEnoughData("Heartbeat history needs to contain at least two value")
 
     self.heartbeat_history = []
@@ -22,11 +22,24 @@ class AccuralFailureDetector(object):
     self.mean = self._mean(self.heartbeat_history)
 
   def _mean(self, arr):
-    return sum(arr) * 1.0 / len(arr)
+    return sum(arr) / float(len(arr))
+
+  def _stdv(self, arr):
+    mean = self._mean(arr)
+    # TODO std lol
+    std = 0
+
+    for elem in arr:
+      std = std + (elem - mean)**2
+
+    std = math.sqrt(std / float(len(arr)-1))
+    return std
 
   def phi(self, t):
     """
     phi = -log10(1 - F(timeSinceLastHeartbeat))
+
+    -log10(1 - (1/2 * (1 + math.erf((timeSinceLastHeartbeat - mean)/(stdev * sqrt(2)))))
 
     F is the CDF of the event distribution. For the exponential distribution,
     the CDF is: 1 - e^(-Lt) and L is given by 1/mean
@@ -37,4 +50,5 @@ class AccuralFailureDetector(object):
     phi = -log10(e^(-t/mean)) = -log(e^(-t/mean)) / log(10) = (t/mean) / log(10)
         = t/mean * 1/ln(10) = t/mean * 0.4342945
     """
-    return t / self.mean * 0.4342945
+
+    return float(t) / float(self._mean(self.heartbeat_history)) * 0.4342945
