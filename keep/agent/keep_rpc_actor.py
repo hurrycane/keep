a28ce08d5bbbe1abd940511760e8d6761a30a6aa
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-20 14:07:59 +0300

    Inject crow and start the hosts actor

--- keep/agent/keep_rpc_actor.py
+++ keep/agent/keep_rpc_actor.py
@@ -1,9 +1,13 @@
 from keep.actors import Actor
 
+from keep.agent.actors import HostsActor
+
 class KeepRpcActor(Actor):
 
-  def __init__(self, context):
+  def __init__(self, crow, context):
     super(KeepRpcActor, self).__init__(context)
+
+    context.actor_of(HostsActor.props(crow=crow))
 
   def on_receive(self, message):
     #actor_ref = self.actor_selection("/KeepRpcActor")
