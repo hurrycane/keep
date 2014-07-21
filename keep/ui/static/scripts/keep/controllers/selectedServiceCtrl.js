6d1639070788575636d63c1a4528d6d1e15c3257
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-21 15:31:12 +0300

    Added ui.bootstrap bindings for angular - so we can open modals that are managed.

--- keep/ui/static/scripts/keep/controllers/selectedServiceCtrl.js
+++ keep/ui/static/scripts/keep/controllers/selectedServiceCtrl.js
@@ -1,11 +1,25 @@
 'use strict';
 
 angular.module('keepUiApp')
-  .controller('SelectedServiceCtrl', function ($scope, Keep) {
+  .controller('ModalDeployCtrl', function ($scope, Keep) {
+
+  });
+
+angular.module('keepUiApp')
+  .controller('SelectedServiceCtrl', function ($scope, $modal, ModalDeployCtrl, Keep) {
 
     var keep = Keep()
 
     $scope.destroyService = function(service){
       new keep.Service().$delete({id: service.id})
+    }
+
+    $scope.deploy = function(service){
+      var modalInstance = $modal.open({
+        templateUrl: '/static/views/modal.deploy.html',
+        controller: ModalDeployCtrl,
+        resolve: {
+        }
+      })
     }
   });
