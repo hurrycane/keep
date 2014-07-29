d87c536892eb2a4fed91ccdd982618c911432242
Bogdan Gaza <bc.gaza@gmail.com>
2014-07-29 19:55:58 +0000

    Made the deploy actor send messages to everyone

--- keep/agent/actors/docker_executor_actor.py
+++ keep/agent/actors/docker_executor_actor.py
@@ -1,12 +1,29 @@
 from keep.actors import Actor
 
+KEEP_SERVICES = "keep-services"
+
 class DockerExecutorActor(Actor):
 
   def __init__(self, crow, polar_client, context):
     super(DockerExecutorActor, self).__init__(context)
 
     self.crow = crow
+    self.polar_client = polar_client
 
   def on_receive(self, message):
-    # needs to update the status in etcd periodically
+    if message["message_type"] == "deploy":
+      self._deploy(message["service"], message["version"])
+
     return "OK"
+
+  def _deploy(self, service_id, version):
+    service_data, stats = self.polar_client.get("%s/%s" % \
+                                                (KEEP_SERVICES, service_id))
+
+    uri = urlparse(absolute_path)
+
+    # connect to every host and tell them to start the container
+    for host in service_data["hosts"]:
+      actor_ref = self.context.actor_selection("actor.etcd://%s/KeepRpcActor/DockerExecutorActor" % host)
+
+      actor_ref.tell({})
